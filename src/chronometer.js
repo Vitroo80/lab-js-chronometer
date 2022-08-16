06c021dbf21a246865b62c5e5dded70cc1bae387
Lucas Vitro <lucasvitro9@gmail.com>
2022-08-16 15:39:46 -0400

    fim :)

--- src/chronometer.js
+++ src/chronometer.js
@@ -39,6 +39,7 @@ class Chronometer {
   }
 
   split() {
-    // ... your code goes here
+    return (this.computeTwoDigitNumber(this.getMinutes()) + ':' +
+      this.computeTwoDigitNumber(this.getSeconds()));
   }
 }
