99c264896df1ba43ba471fc91d161575e669ce28
Lucas Vitro <lucasvitro9@gmail.com>
2022-08-16 15:31:54 -0400

    quase

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,30 +1,41 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() =>{
+      this.currentTime +=1
+      if(printTimeCallback){
+        printTimeCallback()
+      }
+    },1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime/60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime % 60)
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let twoDigits = value.toString()
+    if(twoDigits.length === 1){
+      twoDigits="0"+twoDigits
+      
+    }
+    return twoDigits
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
